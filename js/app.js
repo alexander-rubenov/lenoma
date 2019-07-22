8cc8c81a4557e1c385265f5e425347b8de5610e5
Alexander Rubenov <alexander.rubenov@gmail.com>
2019-07-22 19:34:06 +0300

    Added an adaptive to top menu in Header

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let
+    buttonToOpenCatalogueOnMobileVersion = document.querySelector('.mobile-top-menu__button-link'),
+    catalogueOnMobileVersion = document.querySelector('.mobile-top-menu__sections'),
+    buttonToOpenVerticalSitetoolbar = document.querySelector('.sitetoolbar-vertical__checkbox'),
+    verticalSitetoolbar = document.querySelector('.sitetoolbar__sections');
+
+
+function openVerticalSitetoolbar() {
+    verticalSitetoolbar.classList.toggle('active');
+}
+
+function openCatalogueOnMobileVersion() {
+    if (catalogueOnMobileVersion.classList.contains('active')) {
+        catalogueOnMobileVersion.firstElementChild.classList.toggle('active');
+
+        setTimeout(() => {
+            catalogueOnMobileVersion.classList.toggle('active');
+        }, 400);
+    } else {
+        catalogueOnMobileVersion.classList.toggle('active');
+
+        setTimeout(() => {
+            catalogueOnMobileVersion.firstElementChild.classList.toggle('active');
+        }, 0);
+    }
+}
+
+buttonToOpenCatalogueOnMobileVersion.addEventListener('click', openCatalogueOnMobileVersion);
+buttonToOpenVerticalSitetoolbar.addEventListener('change', openVerticalSitetoolbar);
